80a94b339275d5859ee50f031081e93b4ed323fb
Vardhan Deepak Doshi <vardhan.doshi@sklforest.com>
2025-02-24 10:31:48 +0530

    first commit

---
+++ src/main/java/com/example/kafkaJenkins/KafkaJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaJenkins/KafkaJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
